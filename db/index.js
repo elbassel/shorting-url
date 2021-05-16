9caf054d48bd31d38a0733c742f8f2dc6fa8c997
ahmed_elbassel <elbassel.n13@gmail.com>
2021-05-16 03:08:32 +0200

    Fixing Docker files

--- db/index.js
+++ db/index.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const sampleData = require('./sample-data')
 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
-sampleData();
